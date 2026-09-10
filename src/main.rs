0ffeb6879c71e871d909fb43bfa267942c334569
uvbx <uvbx@proton.me>
2026-09-10 11:24:25 -0500

    added support for MX Anywhere 3s. Added config file, for customising key actions. Added restart script.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,55 @@
 use evdev::{ AttributeSet, EventType, InputEvent, Key, RelativeAxisType };
 use evdev::uinput::VirtualDeviceBuilder;
+
 use std::thread::sleep;
 use std::time::Duration;
 use std::error::Error;
+use std::path::PathBuf;
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    version: String,
+    left_key: String,
+    right_key: String,
+}
+
+fn config_path() -> PathBuf {
+    PathBuf::from(std::env::var("HOME").unwrap()).join(".config/mx2s-tiltkeys/config.json")
+}
+
+fn check_config() -> Result<Config, Box<dyn Error>> {
+    let path: PathBuf = config_path();
+
+    if !path.exists() {
+        let default_config = r#"{
+    "version": "1.2",
+    "left_key": "KEY_VOLUMEUP",
+    "right_key": "KEY_VOLUMEDOWN"
+}
+"#;
+
+        println!("no config found! generating default\n========================================\n{}========================================", &default_config);
+
+        fs::create_dir_all(path.parent().unwrap())?;
+        fs::write(&path, default_config)?;
+    }
+
+    let config_content: String = fs::read_to_string(&path)?;
+    let config: Config = serde_json::from_str(&config_content)?;
+    Ok(config)
+}
+
 
 
 fn get_dev() -> evdev::Device {
     loop {
-        let target_device = evdev::enumerate().find_map(|(_, device)| {
-            let name = device.name()?;
+        let target_device: Option<evdev::Device> = evdev::enumerate().find_map(|(_, device)| {
+            let name: &str = device.name()?;
 
-            if name.contains("MX Anywhere 2S") {
+            if name.contains("MX Anywhere") {
                 Some(device)
             } else {
                 None
@@ -19,7 +58,7 @@ fn get_dev() -> evdev::Device {
         });
 
         if let Some(dev) = target_device {
-                return dev;
+            return dev;
         }
 
         sleep(Duration::from_secs(3));
@@ -29,28 +68,35 @@ fn get_dev() -> evdev::Device {
 
 fn main() -> Result <(), Box<dyn Error>> {
 
-    
-    let mut device = get_dev();
+
+    let config: Config = check_config()?;
+
+    println!("{}", config.left_key);
+    let left_key: Key = config.left_key.parse().map_err(|e| format!("invalid left key set {:?}", e))?;
+    let right_key: Key = config.right_key.parse().map_err(|e| format!("invalid left key set {:?}", e))?;
+
+    println!("version: {} \nleft: {} \nright: {}", config.version, config.left_key, config.right_key);
+
+    let mut device: evdev::Device = get_dev();
 
     let mut keys = AttributeSet::<Key>::new();
-    keys.insert(Key::KEY_VOLUMEDOWN);
-    keys.insert(Key::KEY_VOLUMEUP);
+    keys.insert(left_key);
+    keys.insert(right_key);
 
     let mut virtdev = VirtualDeviceBuilder::new()?
         .name("Wheel_Tilt_Keys")
         .with_keys(&keys)?
         .build()?;
-        
-    loop {
 
-        let disconnected = match device.fetch_events() {
+    loop {
+        let disconnected: bool = match device.fetch_events() {
             Ok(events) => {
                 for event in events {
                     if event.event_type() == EventType::RELATIVE && event.code() == RelativeAxisType::REL_HWHEEL.0 {
                         let key = if event.value() > 0 {
-                            Key::KEY_VOLUMEUP
+                            left_key
                         } else {
-                            Key::KEY_VOLUMEDOWN
+                            right_key
                         };
 
                         virtdev.emit(&[
